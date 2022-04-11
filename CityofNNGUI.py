7b0aa19702265fd443d795528667117210d42083
Jordan Lee <jordanlee2017@gmail.com>
2022-04-11 02:35:39 -0400

    hover test 6

--- CityofNNGUI.py
+++ CityofNNGUI.py
@@ -697,7 +697,7 @@ def employee():
     plots.add_trace(
         go.Pie(values=df_annual['Annual Compensation Package'], labels=labels,
                pull=[i for i in explode[:len(labels)]],
-               hovertemplate='%{label}: %{x:$.,2f} %{percent}'),
+               hovertemplate='%{label}: %{value:$,.2f} %{percent}'),
         row=1, col=2
 
     )
