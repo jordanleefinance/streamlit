47b1e578f91778679fd0710c4fa9421b193f673c
Jordan Lee <jordanlee2017@gmail.com>
2022-04-11 03:08:20 -0400

    bigtset

--- CityofNNGUI.py
+++ CityofNNGUI.py
@@ -718,12 +718,12 @@ def employee():
     plots.update_layout(height=1000, width=1500, legend_title="Legend", legend_font_size=14,
                         legend_title_font_size=19, legend=dict(orientation="v", x=2.35))
 
-    plots.update_annotations(x=0.5, y=1.085, selector={'text': 'Voluntary Benefits'})
-    plots.update_annotations(x=0.5, y=0.475, selector={'text': 'Mandatory Benefits'})
+    plots.update_annotations(x=0.25, y=1.185, selector={'text': 'Voluntary Benefits'})
+    plots.update_annotations(x=0.25, y=0.475, selector={'text': 'Mandatory Benefits'})
     plots.update_annotations(x=1.5, y=1.002, selector={'text': 'Full Compensation Package'})
 
-    plots.data[0].domain = {'x': [0, 1], 'y': [0.2, 0.29]}
-    plots.data[1].domain = {'x': [1, 1], 'y': [0.4, 0.29]}
+    plots.data[0].domain = {'x': [0, 1], 'y': [0.8, 0.29]}
+    plots.data[1].domain = {'x': [0, 1], 'y': [0.98, 0.99]}
     plots.data[2].domain = {'x': [0, 1], 'y': [0.2, 0.29]}
 
     # fig2 = px.bar(new_df, x=new_df.index, y=columns_list, barmode='stack', labels=labels)
