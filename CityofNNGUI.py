484903304fc2e01b6067028076d9a4b03005941d
Jordan Lee <jordanlee2017@gmail.com>
2022-04-11 03:43:16 -0400

    drop

--- CityofNNGUI.py
+++ CityofNNGUI.py
@@ -716,13 +716,13 @@ def employee():
 
     )
     plots.update_layout(height=1000, width=1500, legend_title="Legend", legend_font_size=14,
-                        legend_title_font_size=19, legend=dict(orientation="v", x=2.35))
+                        legend_title_font_size=19, legend=dict(orientation="v", x=1.75))
 
     plots.update_annotations(x=0.25, y=1.185, selector={'text': 'Voluntary Benefits'})
     plots.update_annotations(x=0.25, y=0.525, selector={'text': 'Mandatory Benefits'})
     plots.update_annotations(x=1.5, y=1.002, selector={'text': 'Full Compensation Package'})
 
-    
+
     print(plots.data[2].domain)
 
     # fig2 = px.bar(new_df, x=new_df.index, y=columns_list, barmode='stack', labels=labels)
