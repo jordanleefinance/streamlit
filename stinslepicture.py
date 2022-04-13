c40c4276b1e082db0c28b1acf9bca20800c8d9b2
Jordan Lee <jordanlee2017@gmail.com>
2022-04-13 02:07:43 -0400

    requirements txt fix

--- stinslepicture.py
+++ stinslepicture.py
@@ -4,8 +4,9 @@ import cv2
 
 
 def load_image(file):
-    img = Image.open(file)
-    return img
+    image = Image.open(file)
+    return image
+
 
 image_file = st.file_uploader("Upload Images", type=["png", "jpg", "jpeg"])
 img = cv2.imread(image_file)
@@ -15,13 +16,13 @@ img = cv2.imread(image_file)
 gray_image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
 # Gray Image to Inverted Gray Image
-inverted_gray_image = 255-gray_image
+inverted_gray_image = 255 - gray_image
 
 # Blurring The Inverted Gray Image
 blurred_inverted_gray_image = cv2.GaussianBlur(inverted_gray_image, (19, 19), 0)
 
 # Inverting the blurred image
-inverted_blurred_image = 255-blurred_inverted_gray_image
+inverted_blurred_image = 255 - blurred_inverted_gray_image
 
 # Preparing Photo sketching
 sketch = cv2.divide(gray_image, inverted_blurred_image, scale=256.0)
