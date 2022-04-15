b378ce24b63a304d81ca55fb0e16b67eeab5d9d1
Jordan Lee <jordanlee2017@gmail.com>
2022-04-15 11:22:29 -0400

    test

--- CityofNNGUI.py
+++ CityofNNGUI.py
@@ -65,8 +65,7 @@ if user_type == "Current Employee":
                 "\n\n**This statement is designed to show how much your service is valued by us.**"
 
     st.write(statement, unsafe_allow_html=False)
-
-    user_EIN = st.sidebar.number_input("Enter your EIN", value=8963, step=0, on_change=False)
+    user_EIN = st.sidebar.number_input("Enter your EIN")
     salary = 0.0
     job_title = "Treasurer"
     health_coverage = ''
