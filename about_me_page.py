259df21460f962de1f8e2e183f7dfdebb39d6643
Jordan Lee <jordanlee2017@gmail.com>
2022-12-06 22:50:06 -0500

    Update about me page
    - Fixed formatting
    - Deleted images
    - Reorganized messages

--- about_me_page.py
+++ about_me_page.py
@@ -5,68 +5,58 @@ st.header("About Me")
 with st.container():
     image_col, text_col = st.columns((1, 2))
     with image_col:
-        st.image("https://media-exp1.licdn.com/dms/image/C4D03AQHVHSuW0T27FA/profile-displayphoto-shrink_200_200/0/16"
-                 "07095432597?e=1657756800&v=beta&t=uHa3mlVptW7LmWAoCxRbJEIk8AZ99MBuNHKyPy0wWNA")
+        st.image("https://media-exp1.licdn.com/dms/image/D4E03AQGiCmy0yRdqag/profile-displayphoto-shrink_800_800/0/1667658333213?e=1675900800&v=beta&t=M74I47KgX40ZrtsYObWoR4-ISAQB8cZ3-RGBXRGsao0")
     with text_col:
-        st.subheader("Jordan Lee")
         st.write(
-            "Thank you for checking out my webpage! Below you can find some of my passions, goals, hobbies."
-            "I am a recent graduate from Christopher Newport University with "
-            "an entrepreneurial mindset and strong will to invest into building relationships."
-            " In the next 5 years, I plan on pursing my CFA (Chartered Financial Analyst) and shortly after "
-            "register JMM Group, LLC as an RIA (Registered Investment Advisor). "
+            "Thank you for checking out my webpage! "
+            "I am currently an Analyst for Ballast Consulting Group and recently graduated from "
+            "Christopher Newport University with my Master's in Financial Analysis. "
             "A major detail I learned from my masters is within businesses "
-            "there are systems within systems and I believe a great"
-            " way to incorporate systems thinking into business decision making is through information science."
-
+            "there are systems within systems and translated tremendously "
+            "into my understanding of how to manage clients' needs. "
+            "I have an entrepreneurial mindset and strong will to invest into building relationships."
+            " In the next 5 years, I plan on pursing my CFA and shortly after "
+            "pursue my Series 63, 65, 66, and 79. "
+            "Below you can find some of my apps/projects, passions, goals, hobbies."
         )
+
+with st.container():
+        st.write("""The Total [Compensation Calculator](https://share.streamlit.io/jordanleefinance/streamlit/main/CityofNNGUI.py)
+         was designed for the City of Newport News to attract new employees as well as give current employees a visual of their total compensation.
+         Frontline is the first digital temp staffing platform tailored for green jobs. I was able to
+         explore different ways to help, for example the 
+         [Independent Contractor Time Tracker](https://www.appsheet.com/start/76c16fba-7e57-48a4-ad40-8d17850daa92) and 
+         Park Litter Calculator (Purchased).
+        If you love to workout and hate the hassel of tracking your progress, try out 
+        [Mar Works Out](https://www.appsheet.com/start/198ae0e8-52d5-4428-927a-f0dc6770edaa), where you can personalize
+         your schedule and track progress at your finger tips!""")
+
+
+with st.container():
+    image_col, text_col = st.columns((1, 2))
+    with image_col:
+        st.image("https://images.squarespace-cdn.com/content/v1/58fffa62ebbd1a6b493a5cc2/1493231727410-J4VQU30C6G2FW1L7EIRH/DSC_0964.JPG?format=1000w")
+    with text_col:
+        st.write(""""To become truly a great leader, you must become a servant leader" 
+        CNU Men's lacrosse has given me the opportunity to go abroad to grow the game of lacrosse by going to
+        places where they never heard of it before. [Read more...](https://www.lacrossethenations.org/)
+        A greater passion of mine is to increase the diversity in lacrosse, please take time to consider donating.""")
+
 
 with st.container():
     text_col, image_col = st.columns((2, 2))
     with image_col:
         st.image("https://mediacloud.kiplinger.com/image/private/s--x2_BoIgn--/f_auto,t_primary-image-desktop@1/v1604352227/Investing/stock-market-today-110220.jpg")
     with text_col:
-        st.subheader("Stock Analysis")
-        st.write("""Over time, I've noticed a lot of people don't know much about the stock market or even what a stock
-        is in general. One of my goals is to create an avenue for the average person to be able to learn how to grow 
-        their income based on their own knowledge and confidence. 
-        Click [here](https://www.jmmgroupllc.xyz/templates/products.html) for a glimpse of a couple analysis tools.
-        Click [subscribe](https://www.jmmgroupllc.xyz/templates/subscribe.html) to be redirected to the subscribe page.""")
+        st.write("""One of my goals is to create an avenue for the average person to be able to learn how to grow 
+        their income based on their own knowledge and confidence. Personalize your stock search with my 
+        [Stock Search Web App](https://share.streamlit.io/jordanleefinance/streamlit/main/gitexample.py)! 
+        Click [here](https://www.jmmgroupllc.xyz/templates/products.html) for a glimpse of a couple macro-economic analysis tools.""")
+
 
 with st.container():
-    image_col, text_col = st.columns((1, 2))
-    with image_col:
-        st.image("https://images.squarespace-cdn.com/content/v1/"
-                 "58fffa62ebbd1a6b493a5cc2/1493171975996-XZCUH51COEBWDEL6X5NS/image-asset.jpeg?format=750w")
-    with text_col:
-        st.subheader("Grow the game")
-        st.write("""CNU Men's lacrosse has given me th opportunity to go abroad to grow the game of lacrosse by going to
-        places where they never heard of it before. [Read more...](https://www.lacrossethenations.org/)""")
-
-with st.container():
-    text_col, image_col = st.columns((1, 1))
-    with image_col:
-        st.image("https://www.alphasoftware.com/hs-fs/hubfs/2015/09/"
-                 "background.png?width=1350&height=840&name=background.png")
-    with text_col:
-        st.subheader("Apps")
-        st.write("""The Total [Compensation Calculator](https://share.streamlit.io/jordanleefinance/streamlit/main/CityofNNGUI.py)
-         was designed for the City of Newport News.
-         Frontline is the first digital temp staffing platform tailored for green jobs. I was able to
-         explore different ways to help, for example the 
-         [Independent Contractor Time Tracker](https://www.lacrossethenations.org/) and 
-         [Park Litter Calculator]().
-         Personalize your stock search with my 
-        [Stock Search Web App](https://share.streamlit.io/jordanleefinance/streamlit/main/gitexample.py)!""")
-
-with st.container():
-    image_col, text_col = st.columns((2, 2))
-    with image_col:
-        st.image("https://advantagefamily.com/wp-content/uploads"
-                 "/2018/06/financial-planner-blog-topics-1.png")
-    with text_col:
-        st.subheader("Blog")
-        st.write("""This blog is aimed to give people more knowledge on common financial tools/concepts. Also, this blog 
+    st.write("""[Research & Investment blog coming soon...](https://www.jmmgroupllc.xyz/templates/blog.html)
+    This blog is aimed to give people more knowledge on common financial tools/concepts. Also, this blog 
         gives you an opportunity to provide any feedback on my website, strategies, etc. I have yet to start posting due time
         constraints, but I am gathering ideas for what topics to discuss and would love your input! 
-        [Coming soon...](https://www.jmmgroupllc.xyz/templates/blog.html)""")
+        """)
