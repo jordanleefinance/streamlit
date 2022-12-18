3643c8bc8a39465130330793fff0dfef0a8cb7b1
Jordan Lee <jordanlee2017@gmail.com>
2022-12-18 01:44:17 -0500

    First Update AM Calculator

--- Finance/AM Calculator.py
+++ Finance/AM Calculator.py
@@ -1,7 +1,6 @@
 import pandas as pd
 import streamlit as st
-
-
+import datetime
 
 def get_monthly_pmt(loan_amt, r, n):
     """ calculate monthly payment
@@ -11,6 +10,26 @@ def get_monthly_pmt(loan_amt, r, n):
     """
     return loan_amt * r * (1 + r) ** n / ((1 + r) ** n - 1)
 
+
+
+
+
+
+
+
+with st.form("Loan Details"):
+    st.write("Please provide the following:")
+    loan_amt = st.number_input("Loan Amount", step=1)
+    interest = st.number_input("Interest Rate", step=0.0001, format="%i")
+    pper = st.number_input("Payments per Period", value=12)
+    start_date = st.date_input("Start Date")
+    end_date = st.date_input("End Date")
+
+    periods = end_date.year - start_date.year * pper
+    print(periods)
+    button = st.form_submit_button("Submit")
+    if button:
+        get_monthly_pmt(loan_amt, interest, periods)
 
 def get_principle(loan_amt, A, r, t):
     """ calculate principle for month t
